c9d05778ee521a481c9b9c8dc7e58b1c861ae23c
systems <systems@hometownroofingtx.com>
2026-08-09 15:43:36 +0800

    set with

--- dmb.web/src/styles/main_style.tsx
+++ dmb.web/src/styles/main_style.tsx
@@ -397,7 +397,11 @@ export const agenticPageSx = {
 
   panelBody: {
     ...agenticSurfaceSx.panel,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
     maxWidth: "100%",
+    alignItems: "center",
     p: { xs: 2.5, sm: 3 },
   } satisfies SxProps<Theme>,
 
