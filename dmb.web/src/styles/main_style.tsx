a98f1afcb39f4675f3c1b2af8d4a39817e5bde90
systems <systems@hometownroofingtx.com>
2026-08-09 15:20:58 +0800

    fixed styling issue on edit mode

--- dmb.web/src/styles/main_style.tsx
+++ dmb.web/src/styles/main_style.tsx
@@ -397,7 +397,7 @@ export const agenticPageSx = {
 
   panelBody: {
     ...agenticSurfaceSx.panel,
-    p: { xs: 2.5, sm: 3 },
+    // p: { xs: 2.5, sm: 3 },
   } satisfies SxProps<Theme>,
 
   loadingState: {
